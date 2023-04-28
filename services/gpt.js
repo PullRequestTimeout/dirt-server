7c7cdf724769a32627e5a0940b1c5fb170bda1ed
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-04-27 20:27:06 -0700

    :construction: Got weather object made after much hassle. Apparently I'm bad at async/await

---
+++ services/gpt.js
@@ -0,0 +1,62 @@
+import cron from "node-cron";
+import fetch from "node-fetch";
+import Trails from "../models/Trails.js";
+import weatherDataCall from "./weather.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_TRAILS = process.env.MONGO_TRAILS
+const MONGO_FORECASTS = process.env.MONGO_FORECASTS
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY
+
+// Cron job every day at 3am contains...
+// (cron job can be wrapped around function last)
+    // Fetch recent weather data and massage it into a useful object for OpenAI api to consume.
+
+    // Fetch the three trail collections and store each of them in a variable, which will be JSON array, make an array of the three arrays
+    // Iterate through each object in each array and do...
+        // Another fetch request, which calls the OpenAI model with data points; 
+            // trail object 
+            // recent weather data for the relevent location
+            // instruction for the model
+
+            // And then makes a new DB entry for each of the returned forecasts with 'trailName', 'starRating', and 'description'
+
+
+// Pieces of the puzzle:
+
+// This will run the task at 3am every day
+// cron.schedule('0 3 * * *', () => {
+//     // your task code here
+// });
+
+
+const retrieveWeather = async () => {
+    const weather = { trail: [] , rossland: [] , castlegar: [] }
+
+    for (const key in weather) {
+        await weatherDataCall(key)
+        .then(data => formatWeatherData(data))
+        .then(data => weather[key] = data)
+    }
+
+    console.log(weather)
+}
+
+
+function formatWeatherData(data) {
+    const result = [];
+    const daysCount = data.daily.time.length;
+  
+    for (let i = 0; i < daysCount; i++) {
+      result.push({
+        time: data.daily.time[i],
+        temperature_2m_max: data.daily.temperature_2m_max[i],
+        temperature_2m_min: data.daily.temperature_2m_min[i],
+        rain_sum: data.daily.rain_sum[i],
+        snowfall_sum: data.daily.snowfall_sum[i]
+      });
+    }
+  
+    return result;
+}
