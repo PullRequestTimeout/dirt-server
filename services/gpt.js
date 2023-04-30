461fe69bab16ab6c50aba6aebaf5c7a280826319
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-04-30 10:36:36 -0700

    :construction: Loop function for traildb

--- services/gpt.js
+++ services/gpt.js
@@ -9,9 +9,12 @@ const MONGO_TRAILS = process.env.MONGO_TRAILS
 const MONGO_FORECASTS = process.env.MONGO_FORECASTS
 const OPEN_AI_KEY = process.env.OPEN_AI_KEY
 
+// Just for testing, remove after
+import mongoose from "mongoose";
+
 // Cron job every day at 3am contains...
 // (cron job can be wrapped around function last)
-    // Fetch recent weather data and massage it into a useful object for OpenAI api to consume.
+    // Fetch recent weather data and massage it into a useful object for OpenAI api to consume. ✅
 
     // Fetch the three trail collections and store each of them in a variable, which will be JSON array, make an array of the three arrays
     // Iterate through each object in each array and do...
@@ -31,7 +34,7 @@ const OPEN_AI_KEY = process.env.OPEN_AI_KEY
 // });
 
 
-async function retrieveWeather () {
+async function retrieveWeather() {
     const weather = { trail: [] , rossland: [] , castlegar: [] }
 
     for (const key in weather) {
@@ -61,4 +64,22 @@ function formatWeatherData(data) {
     return result;
 }
 
-retrieveWeather()
+// Fetch the three trail collections and store each of them in a variable, which will be JSON array, make an array of the three arrays
+// This wont work unless connected to the mongodb, make sure IP is whitelisted in mongo 'Network Access' tab
+
+mongoose.connect(MONGO_TRAILS).then(() => console.log("Trails database connected 👍"))
+
+async function getTrailArrays() {
+    const rosslandTrails = await Trails.RosslandDB.find();
+    const trailTrails = await Trails.TrailDB.find();
+    const castlegarTrails = await Trails.CastlegarDB.find();
+    
+    const trailArrays = [rosslandTrails, trailTrails, castlegarTrails];
+
+    // This loop is the end goal, needs to iterate through each item and send it to gpt with the params
+    trailArrays.forEach(array => {
+        array.forEach(item =>{
+            console.log("item:", item)
+        })
+    })
+}
