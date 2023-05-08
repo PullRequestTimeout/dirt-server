8fc5cebd04eb6a86b3d13ec7b44d752dc26a8ef1
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-07 20:49:06 -0700

    :art: Welcome message and Prettier doing its thing

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-import express from "express";
-const app = express();
-const PORT = 6969;
+//     Welcome!!   >(.)__                                                       //
+//                  (___/   Roast my code, I need to learn.                     //
+//                 ~~~~~~~                                @PullRequestTimeout   //
+// -----------------------------------------------------------------------------//
 
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express"
+const app = express()
+const PORT = 6969
+
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
 
 // To enable localhost development, not sure if I need this in prod still
-app.use(cors());
+app.use(cors())
 
 // Routes
-import updateRoute from "./routes/update.js";
-app.use("/update", updateRoute);
-import trailsRoute from "./routes/trails.js";
-app.use("/trails", trailsRoute);
-import forecastsRoute from "./routes/forecasts.js";
-app.use("/forecasts", forecastsRoute);
+import updateRoute from "./routes/update.js"
+app.use("/update", updateRoute)
+import trailsRoute from "./routes/trails.js"
+app.use("/trails", trailsRoute)
+import forecastsRoute from "./routes/forecasts.js"
+app.use("/forecasts", forecastsRoute)
 
 // environment variables
 const OPEN_AI_KEY = process.env.OPEN_AI_KEY
@@ -24,10 +29,16 @@ const MONGO_TRAILS = process.env.MONGO_TRAILS
 
 // API home. Maybe should return the readme file.
 app.get("/", (req, res) => {
-    res.status(200).send(console.log("Welcome to Dirt Server. Please use appropriate location routing to recieve JSON data."))
+    res.status(200).send(
+        console.log(
+            "Welcome to Dirt Server. Please use appropriate location routing to recieve JSON data."
+        )
+    )
 })
 
-mongoose.connect(MONGO_TRAILS).then(() => console.log("Trails database connected 👍"))
+mongoose
+    .connect(MONGO_TRAILS)
+    .then(() => console.log("Trails database connected 👍"))
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}/. Niiiiice.`)
