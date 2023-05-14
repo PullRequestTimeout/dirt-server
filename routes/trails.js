b3d0067133964f5e8f23ca539cd052d3766d71d8
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-14 14:55:29 -0700

    :fire: Removed repetitive routing and made it dynamic

--- routes/trails.js
+++ routes/trails.js
@@ -30,38 +30,4 @@ function locationDB(location) {
     }
 }
 
-// router.get("/rossland", async (req, res) => {
-//     await mongoose.connect(MONGO_TRAILS)
-//     try {
-//         const rosslandTrails = await Trails.RosslandDB.find()
-//         res.json(rosslandTrails)
-//         await mongoose.disconnect()
-//     } catch (error) {
-//         res.json({ message: error })
-//     }
-//     await mongoose.disconnect()
-// })
-
-// router.get("/trail", async (req, res) => {
-//     await mongoose.connect(MONGO_TRAILS)
-//     try {
-//         const trailTrails = await Trails.TrailDB.find()
-//         res.json(trailTrails)
-//     } catch (error) {
-//         res.json({ message: error })
-//     }
-//     await mongoose.disconnect()
-// })
-
-// router.get("/castlegar", async (req, res) => {
-//     await mongoose.connect(MONGO_TRAILS)
-//     try {
-//         const castlegarTrails = await Trails.CastlegarDB.find()
-//         res.json(castlegarTrails)
-//     } catch (error) {
-//         res.json({ message: error })
-//     }
-//     await mongoose.disconnect()
-// })
-
 export default router
